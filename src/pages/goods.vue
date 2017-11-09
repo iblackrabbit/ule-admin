bca42551689034467a662963306defbb09bd202d
384731580 <384731580@qq.com>
2017-11-09 16:39:07 +0800

    11091639

--- src/pages/goods.vue
+++ src/pages/goods.vue
@@ -1,99 +1,70 @@
-  <template>
+<template>
   <div id="goods">
     <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-  <el-menu-item index="1">处理中心</el-menu-item>
-  <el-submenu index="2">
-    <template slot="title">我的工作台</template>
-    <el-menu-item index="2-1">选项1</el-menu-item>
-    <el-menu-item index="2-2">选项2</el-menu-item>
-    <el-menu-item index="2-3">选项3</el-menu-item>
-  </el-submenu>
-  <el-menu-item index="3"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
-</el-menu>
+      <el-menu-item index="1" @click="changePC">优惠券</el-menu-item>
+      <el-submenu index="2" @click="changeWS">
+        <template slot="title">工作区</template>
+        <el-menu-item index="2-1">选项1</el-menu-item>
+        <el-menu-item index="2-2">选项2</el-menu-item>
+        <el-menu-item index="2-3">选项3</el-menu-item>
+      </el-submenu>
+      <el-menu-item index="3" @click="changeOM">商品列表</el-menu-item>
+    </el-menu>
 
-  <div class="search">
-<el-input v-model="input" placeholder="请输入内容" class="search-input"></el-input>
- <el-button type="primary" class="search-button">查询</el-button>
+
+    <Tickets v-if="isShow===1"></Tickets>
+    <Workspace v-if="isShow===2"></Workspace>
+    <Goodslist v-if="isShow===3"></Goodslist>
+
   </div>
+</template>
 
+<script>
+  import Tickets from "./tickets.vue";
+  import Workspace from "./workspace.vue";
+  import Goodslist from "./goodslist.vue";
 
-
-    <el-table
-      :data="tableData"
-      style="width: 100%">
-      <el-table-column
-        prop="date"
-        label="日期"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="name"
-        label="姓名"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="地址">
-      </el-table-column>
-    </el-table>
-    <div class="block">
-      <p></p>
-  <el-pagination
-    layout="prev, pager, next"
-    :total="50">
-  </el-pagination>
-</div>
-  </div>
-  </template>
-
-  <script>
-export default {
-  data() {
-    return {
-      activeIndex: "1",
-      activeIndex2: "1",
-      tableData: [
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 弄"
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1519 弄"
-        },
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1516 弄"
-        }
-      ],
-      input: ""
-    };
-  },
-  methods: {
-    handleSelect(key, keyPath) {
-      console.log(key, keyPath);
+  export default {
+    data() {
+      return {
+        activeIndex: "1",
+        activeIndex2: "1",
+        isShow: 1
+      };
+    },
+    components: {
+      Tickets,
+      Workspace,
+      Goodslist
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      changePC() {
+        this.isShow = 1
+      },
+      changeWS() {
+        this.isShow = 2
+      },
+      changeOM() {
+        this.isShow = 3
+      }
     }
-  }
-};
+  };
 </script>
 <style>
-.search {
-  width: 300px;
-  margin-top: 20px;
-  float: right;
-}
-.search-input {
-  width: 200px;
-}
-.search-button {
-  width: 70px;
-}
+  .search {
+    width: 300px;
+    margin-top: 20px;
+    float: right;
+  }
+
+  .search-input {
+    width: 200px;
+  }
+
+  .search-button {
+    width: 70px;
+  }
 </style>
---
+++ src/pages/tickets.vue
@@ -0,0 +1,60 @@
+<template lang="html">
+    <div class="processcenter">
+          <div class="search">
+      <el-input v-model="input" placeholder="请输入内容" class="search-input"></el-input>
+      <el-button type="primary" class="search-button">查询</el-button>
+    </div>
+
+
+
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column prop="date" label="日期" width="180">
+      </el-table-column>
+      <el-table-column prop="name" label="姓名" width="180">
+      </el-table-column>
+      <el-table-column prop="address" label="地址">
+      </el-table-column>
+    </el-table>
+    <div class="block">
+      <p></p>
+      <el-pagination layout="prev, pager, next" :total="50">
+      </el-pagination>
+    </div>
+    </div>
+</template>
+
+<script>
+   export default {
+    data() {
+      return {
+        tableData: [{
+            date: "2016-05-02",
+            name: "王小虎",
+            address: "上海市普陀区金沙江路 1518 弄"
+          },
+          {
+            date: "2016-05-04",
+            name: "王小虎",
+            address: "上海市普陀区金沙江路 1517 弄"
+          },
+          {
+            date: "2016-05-01",
+            name: "王小虎",
+            address: "上海市普陀区金沙江路 1519 弄"
+          },
+          {
+            date: "2016-05-03",
+            name: "王小虎",
+            address: "上海市普陀区金沙江路 1516 弄"
+          }
+        ],
+        input: ""
+      };
+    }
+ 
+  }
+  
+
+
+
+</script>
